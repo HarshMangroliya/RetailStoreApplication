2cca81eeefa8b7c96c7cf13919abebce6ea048ba
Harsh Mangroliya <harshmangroliya0@gmail.com>
2023-02-23 13:15:30 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,126 @@
+import RetailPack.UserOperations;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean f1 = true;
+        while(f1) {
+            // The main menu is displayed in the beginning, this menu is wrapped inside an infinite while loop,
+            // It will break when the user chooses to Exit the program
+            System.out.println("\n1. Register");
+            System.out.println("2. Login");
+            System.out.println("3. Forgot password");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = UserOperations.scanner.nextInt();
+
+            /** Nested switch begins here. */
+            switch (choice) {
+                case 1:
+                    // The register method from the UserOperations class is called
+                    UserOperations.register();
+                    break;
+                case 2:
+                    // If the login method from the UserOperations class returns true, the user is allowed to move ahead
+                    /*
+                    if (UserOperations.login()) {
+                        boolean f2 = true;
+                        while(f2){
+                            // The menu is displayed after the user has successfully logged in,
+                            // This menu is wrapped inside an infinite while loop
+                            System.out.println("\nWelcome, "+UserOperations.active.getFullname());
+                            bookstore.displayBooks();
+                            System.out.println("\n1. Show Profile");
+                            System.out.println("2. Purchase");
+                            System.out.println("3. Logout");
+                            System.out.print("Enter your choice: ");
+                            choice = UserOperations.scanner.nextInt();
+
+                            switch (choice) {
+                                case 1:
+                                    boolean f3 = true;
+                                    boolean flag = false;
+                                    while(f3){
+                                        // This menu is displayed after the user selects the Show Profile option
+                                        // This menu is also wrapped inside an infinite while loop
+                                        System.out.println("\nPurchase History :");
+
+                                        // The complete list of the books bought by the user is displayed from the purchaseDB ArrayList
+                                        for(int i = 0 ; i < bookstore.purchaseDB.size() ; i++){
+                                            PurchaseDetails pd =  bookstore.purchaseDB.get(i);
+                                            if(pd.returnUsername().equals(UserOperations.active.getUsername())){
+                                                if(!flag)
+                                                    System.out.println("Book ID\tBook Name\t\tPrice");
+
+                                                // The printRecord method is called which returns the books purchased by the current user
+                                                pd.printRecord();
+                                                flag = true;
+                                            }
+                                        }
+                                        // The following if block handles the case when there are no books purchased by the current user
+                                        if(!flag)
+                                            System.out.println("\nNo record found");
+
+                                        // The wallet balance of the current user is displayed
+                                        System.out.println("\nYour wallet balance : " + UserOperations.active.getBalance());
+
+                                        // The following menu is displayed to the user
+                                        System.out.println("\n1. Back to catalog");
+                                        System.out.println("2. Logout");
+                                        System.out.print("Enter your choice: ");
+                                        choice = UserOperations.scanner.nextInt();
+                                        switch (choice) {
+                                            case 1:
+                                                f3 = false;
+                                                break;
+                                            case 2:
+                                                if(UserOperations.logout()) {
+                                                    System.out.println("\nUser logged out successfully.");
+                                                    f3 = false;
+                                                    f2 = false;
+                                                }
+                                                break;
+                                            default:
+                                                System.out.println("\nInvalid choice.");
+                                                break;
+                                        }
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("\n");
+
+                                    // All the books are displayed that are available in the books ArrayList in the BookStore class
+                                    bookstore.displayBooks();
+
+                                    // The user is asked for the book id for purchasing
+                                    UserOperations.purchase(bookstore);
+                                    break;
+                                case 3:
+                                    if(UserOperations.logout()) {
+                                        System.out.println("\nUser logged out successfully.");
+                                        f2 = false;
+                                    }
+                                    break;
+                                default:
+                                    System.out.println("\nInvalid choice.");
+                                    break;
+                            }
+                        }
+                    }
+                    */
+                    break;
+                case 3:
+                    //UserOperations.printUser();
+                    UserOperations.resetPassword();
+                    //UserOperations.printUser();
+                    break;
+                case 4:
+                    f1 = false;
+                    System.out.println("\nProgram exit successful.");
+                    break;
+                default:
+                    System.out.println("\nInvalid choice.");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/RetailPack/UserOperations.java
@@ -0,0 +1,192 @@
+package RetailPack;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class UserOperations {
+    public static HashMap<String, User> users = new HashMap<>();
+
+    public  static Scanner scanner = new Scanner(System.in);
+
+    public static User active;
+
+    public static void register() {
+        System.out.print("\nEnter your full name: ");
+        String fname = null, lname = null;
+        fname = scanner.next();
+        lname = scanner.next();
+        String fullname = fname + " " + lname;
+
+        System.out.print("Enter your username: ");
+        String username = scanner.next();
+
+        User user = UserOperations.users.get(username);
+        if(user != null && user.getUsername().equals(username)){
+            boolean flag = true;
+            while(flag) {
+                System.out.println("\nUser already exists.");
+                System.out.print("Enter your username: ");
+                username = scanner.next();
+
+                if(!user.getUsername().equals(username))
+                    flag = false;
+            }
+        }
+
+
+        String password1 = null ,password2 = null;
+
+        boolean f1 = true;
+        while(f1) {
+            //uncomment the following lines for running in intelliJ IDEA
+            System.out.print("Enter your password: ");
+            password1 = scanner.next();
+            System.out.print("Re-enter your password: ");
+            password2 = scanner.next();
+
+            //uncomment the following lines for running in VS Code
+            //Console cnsl = System.console();
+            //if(cnsl == null) {
+            //    System.out.println("No console available.");
+            //}
+            //char[] passwd1 = cnsl.readPassword("Enter your password: ");
+            //password1 = new String(passwd1);
+            //char[] passwd2 = cnsl.readPassword("Re-enter your password: ");
+            //password2 = new String(passwd2);
+
+            if(password1.equals(password2))
+                f1=false;
+            else
+                System.out.println("\nPlease enter same password\n");
+        }
+
+        user = new User(username, password1, fullname, 50);
+        UserOperations.users.put(username, user);
+        System.out.println("\nUser registered successfully.");
+    }
+
+    /*** Method for user login */
+    public static boolean login() {
+        System.out.print("\nEnter your username: ");
+        String username = scanner.next();
+
+        //uncomment the following lines for running in intelliJ IDEA
+        System.out.print("Enter your password: ");
+        String password = scanner.next();
+
+        //uncomment the following lines for running in VS Code
+        //Console cnsl = System.console();
+        //if(cnsl == null) {
+        //    System.out.println("No console available.");
+        //}
+        //char[] passwd = cnsl.readPassword("Enter password: ");
+        //String password = new String(passwd);
+
+        User user = UserOperations.users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            System.out.println("\nLogin successfully.");
+            UserOperations.active = user;
+            return true;
+        } else {
+            System.out.println("\nLogin failed. Invalid username or password.");
+        }
+        return false;
+    }
+
+    /**
+     * Method for purchasing books
+     * This method accepts an object to the BookStore class
+     **/
+    /*public static void purchase(BookStore b) {
+        // The wallet balance of the current user is displayed
+        System.out.println("Your wallet balance is : " + active.getBalance());
+
+        // User is asked to enter the Book ID for purchasing the book
+        System.out.print("\nEnter the Book ID for purchasing: ");
+        int bid = scanner.nextInt();
+
+        // The sellBook method is called from the BookStore class
+        int result = b.sellBook(bid, UserOperations.active);
+
+        // Depending on the returned value form the sellBook method (-1, 1, or 0) a message is displayed to the current user
+        if(result == 1) {
+            System.out.println("\nBook Purchased successfully");
+        }
+        else if (result == -1) {
+            System.out.println("Your wallet balance is not sufficient");
+        }
+        else {
+            System.out.println("Book is out of stock");
+        }
+    }*/
+
+    /**
+     * Method for resetting password
+     * This method takes the fullname, username of the user for validation before allowing to reset the password
+     **/
+    public static boolean resetPassword() {
+        System.out.print("\nEnter your fullname: ");
+        String fname = null , lname = null;
+        fname = scanner.next();
+        lname =  scanner.next();
+        String fullname = fname+" "+lname;
+        System.out.print("Enter your username: ");
+        String username = scanner.next();
+        User user = UserOperations.users.get(username);
+
+        /*** Only the existing user can reset the password. */
+        if (user != null && user.getUsername().equals(username) && user.getFullname().equals(fullname)) {
+            boolean flag = true;
+            while(flag) {
+                //uncomment the following lines for running in intelliJ IDEA
+                System.out.print("Enter your new password: ");
+                String password1 = scanner.next();
+                System.out.print("Re-enter your new password: ");
+                String password2 = scanner.next();
+
+                //uncomment the following lines for running in VS Code
+                //Console cnsl = System.console();
+                //if(cnsl == null) {
+                //    System.out.println("No console available.");
+                //}
+                //char[] passwd1 = cnsl.readPassword("Enter your new password: ");
+                //String password1 = new String(passwd1);
+                //char[] passwd2 = cnsl.readPassword("Re-enter your new password: ");
+                //String password2 = new String(passwd2);
+
+                if(password1.equals(password2)){
+                    flag = false;
+                    System.out.println("\nPassword reset successfully.");
+                    user.setPassword(password1);
+                }
+                else
+                    System.out.println("\nPlease enter same password.");
+            }
+            return true;
+        }
+        System.out.println("\nNo such user exists.");
+        return false;
+    }
+
+    /** This method sets the current user object as null and logs the user out */
+    public static boolean logout() {
+        UserOperations.active = null;
+        return true;
+    }
+
+    /*** HashMap iterator for displaying all the users. (for debugging purpose only) */
+    public static void printUser() {
+        Iterator<Map.Entry<String, User>> new_Iterator = UserOperations.users.entrySet().iterator();
+
+        while (new_Iterator.hasNext()) {
+            Map.Entry<String, User> new_Map = (Map.Entry<String, User>)
+                    new_Iterator.next();
+            User temp = new_Map.getValue();
+
+            // Displaying HashMap
+            System.out.println(new_Map.getKey() + "  fn:" + temp.getFullname() + " u:" + temp.getUsername() + " p:" + temp.getPassword() + "\n");
+        }
+    }
+}
