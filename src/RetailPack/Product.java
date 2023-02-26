6dca3430989d0c0d03af1024fd089e0a94e03def
adityakumaar <[kunalgauravsrivastava@gmail.com]>
2023-02-26 22:05:37 +0530

    Added comments to Product class

--- src/RetailPack/Product.java
+++ src/RetailPack/Product.java
@@ -1,14 +1,21 @@
 package RetailPack;
 
 public class Product {
+    // variable for product ID
     public int productID;
+    // variable for product name
     public String productName;
+    // variable for product quantity
     public int qty;
+    // variable for product return period
     public int returnPeriod;
+    // variable for product base price
     private double  basePrice;
+    // variable for product sell price
     public double sellPrice;
 
-    public Product(int ProductID,String ProductName,int Qty,int returnPeriod,double basePrice,double sellPrice){
+    // method for setting the product details
+    public Product(int ProductID, String ProductName, int Qty, int returnPeriod, double basePrice, double sellPrice) {
         this.productID = ProductID;
         this.productName = ProductName;
         this.qty = Qty;
