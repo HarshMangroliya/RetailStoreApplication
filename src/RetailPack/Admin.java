ee395a605ce1490994133b8901eb692a8c446270
adityakumaar <[kunalgauravsrivastava@gmail.com]>
2023-02-26 22:01:55 +0530

    Added comments to admin class.

--- src/RetailPack/Admin.java
+++ src/RetailPack/Admin.java
@@ -2,7 +2,7 @@ package RetailPack;
 
 public class Admin extends ParentUser implements AdminInterface {
 
-    public Admin(String u, String p, String fname,int utype) {
+    public Admin(String u, String p, String fname, int utype) {
         username = u;
         password = p;
         fullname = fname;
