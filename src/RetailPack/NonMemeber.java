aa5ec28838adc9eb1267d604e8d6515ebd83c47e
adityakumaar <[kunalgauravsrivastava@gmail.com]>
2023-02-26 22:04:09 +0530

    Added comments to NonMember class

--- src/RetailPack/NonMemeber.java
+++ src/RetailPack/NonMemeber.java
@@ -1,12 +1,11 @@
 package RetailPack;
 
 public class NonMemeber extends User {
-
-    public NonMemeber(String username, String password, String fullname, boolean isMember, int balance,int utype) {
+    public NonMemeber(String username, String password, String fullname, boolean isMember, int balance, int utype) {
         super(username, password, fullname, isMember, balance,utype);
-
     }
 
+    // The getDiscount member calculates the discount for the non-member customers
     @Override
     double getDiscount() {
         return 0;
