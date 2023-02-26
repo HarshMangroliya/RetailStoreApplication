a1e7297d09375848d4ea521390edee15b462d4b8
adityakumaar <[kunalgauravsrivastava@gmail.com]>
2023-02-26 22:03:35 +0530

    Added comments to AdminInterface interface

--- src/RetailPack/AdminInterface.java
+++ src/RetailPack/AdminInterface.java
@@ -1,7 +1,6 @@
 package RetailPack;
 
+// This AdminInterface implements the method for calculating the fines.
 public interface AdminInterface {
-
     void calculateFine(User customer, Product product);
-
 }
